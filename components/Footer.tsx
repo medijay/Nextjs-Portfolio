8441786424543f3570c3defa05df849e6e4d1aa3
Ahmed Adekunle <medijay99@yahoo.com>
2024-07-02 09:58:50 +0100

    styling links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 import { FaLocationArrow } from "react-icons/fa6";
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
-
+import SocialFollow from "./ui/SocialFollow"
 
 const Footer = () => {
   return (
@@ -36,16 +38,18 @@ const Footer = () => {
           Copyright © 2024 Ahmed Adekunle
         </p>
 
+          
         <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((info) => (
+          <SocialFollow />
             <div
-              key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              
-              <img src={info.img} alt="icons" width={20} height={20} />
+
+
+            <div>
+      
+          </div>    
             </div>
-          ))}
         </div>
       </div>
     </footer>
---
+++ components/ui/SocialFollow.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa"
+
+const SocialFollow = () => {
+    return (
+        <div >
+         <a href="https://www.github.com/medijay">
+        <FaGithub  />
+         </a>
+         <a href="https://www.twitter.com">
+        <FaTwitter  />
+         </a>
+         <a href="https://www.twitter.com">
+        <FaLinkedin />
+         </a>
+        </div>
+    )
+}
+
+export default SocialFollow
