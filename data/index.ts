5a8265e39d62282c502177b42fd34ee9571a98ec
Ahmed Adekunle <medijay99@yahoo.com>
2024-07-05 13:22:51 +0100

    updated data

--- data/index.ts
+++ data/index.ts
@@ -203,21 +203,3 @@ export const navItems = [
     },
   ];
   
-  export const socialMedia = [
-    {
-      id: 1,
-      img: "/git.svg",
-
-     
-    
-       },
-    {
-      id: 2,
-      img: "/twit.svg",
-    },
-    {
-      id: 3,
-      img: "/link.svg/",
-
-    },
-  ];
