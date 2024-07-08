5638120d89a73c9c81641445a7984e2856346662
Ahmed Adekunle <medijay99@yahoo.com>
2024-07-08 15:32:51 +0100

    updated clients review

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -111,9 +111,7 @@ export const InfiniteMovingCards = ({
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
-                {/* add this div for the profile img */}
                 <div className="me-3">
-                  <img src="/profile.svg" alt="profile" />
                 </div>
                 <span className="flex flex-col gap-1">
                   
