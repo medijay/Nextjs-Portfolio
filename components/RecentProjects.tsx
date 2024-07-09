671aab7403244c761fac7030d87ed9fea0a75e91
Ahmed Adekunle <medijay99@yahoo.com>
2024-07-09 13:35:03 +0100

    recent commit

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -3,10 +3,11 @@
 
 import { FaLocationArrow } from "react-icons/fa6";
 
-import { projects } from "@/data";
+import { navItems, projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
 
 const RecentProjects = () => {
+
   return (
     <div className="py-20">
       <h1 className="heading">
